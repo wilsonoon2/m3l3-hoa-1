b673007a6f3224f98bb63e636e4f61cdfe860067
wilsonoon2 <wilson.oontb@outlook.com>
2026-05-02 12:06:31 +0800

    2nd Terraform project

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = ""  #Change this
+    key    = "m3l2/terraform.tfstate"  #Change this
     region = "ap-southeast-1"
   }
 }
